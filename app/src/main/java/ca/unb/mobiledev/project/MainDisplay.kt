37e244c7823ec3b74cf33ff7361da31d7166db93
Shashwat Shah <shashwatshah08@yahoo.com>
2022-11-03 18:47:52 -0400

    content changes with change in active tab

--- app/src/main/java/ca/unb/mobiledev/project/MainDisplay.kt
+++ app/src/main/java/ca/unb/mobiledev/project/MainDisplay.kt
@@ -1,16 +1,32 @@
 package ca.unb.mobiledev.project
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
 import android.view.View
 import android.widget.Button
+import android.widget.MediaController
 import android.widget.ProgressBar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.get
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainDisplay: AppCompatActivity() {
+class MainDisplay: AppCompatActivity(){
+
+    lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigatin_view)
+        setupWithNavController(bottomNavigationView, navController)
     }
+
+
 
 }
