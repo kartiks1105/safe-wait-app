27e6847241b427c84f01a45ab28b7ddc45de28ad
Shashwat Shah <shashwatshah08@yahoo.com>
2022-12-06 21:46:19 -0500

    driver information done

---
+++ app/src/main/java/ca/unb/mobiledev/project/Driver.kt
@@ -0,0 +1,5 @@
+package ca.unb.mobiledev.project
+
+data class Driver(
+    var driverInformation: DriverInformation
+)
--- app/src/main/java/ca/unb/mobiledev/project/LoginScreen.kt
+++ app/src/main/java/ca/unb/mobiledev/project/LoginScreen.kt
@@ -9,15 +9,11 @@ import android.widget.EditText
 import android.widget.ProgressBar
 import android.widget.Toast
 import ca.unb.mobiledev.project.databinding.ActivityMainBinding
-import com.fasterxml.jackson.databind.ObjectMapper
-import org.json.JSONObject
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-import java.net.HttpURLConnection
-import java.net.URL
+import java.util.*
+import kotlin.concurrent.schedule
 
 class LoginScreen : AppCompatActivity() {
 
@@ -27,20 +23,28 @@ class LoginScreen : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val profileType = intent.extras?.get("ProfileType")
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(R.layout.get_student_id)
+        setContentView(R.layout.login_form)
         var button = findViewById<Button>(R.id.verify)
         button.setOnClickListener {
             val loading = findViewById<ProgressBar>(R.id.progressBar)
             button.text = ""
             button.isEnabled = false
             loading.visibility = View.VISIBLE
-            if (profileType == "Student") {
-                //verifyStudentInfo(this, loading, button).start()
-                getStudentInfo()
-            } else if (profileType == "Driver") {
-                verifyDriverInfo(this, loading, button).start()
-            } else {
-                throw Exception("ProfileType Mismatched")
+            when (profileType) {
+                "Student" -> {
+                    Timer().schedule(2000){
+                        getStudentInfo()
+                    }
+
+                }
+                "Driver" -> {
+                    Timer().schedule(2000){
+                        getDriverInformation()
+                    }
+                }
+                else -> {
+                    throw Exception("ProfileType Mismatched")
+                }
             }
         }
     }
@@ -56,11 +60,15 @@ class LoginScreen : AppCompatActivity() {
             override fun onResponse(call: Call<Student?>, response: Response<Student?>) {
                 val studentInformation = response.body()?.studentInformation
                 if (studentInformation?.student_id != null) {
-                    var intent = Intent(this@LoginScreen, MainDisplay::class.java)
+                    var intent = Intent(this@LoginScreen, StudentDisplay::class.java)
                     this@LoginScreen.startActivity(intent)
                 } else {
                     studentId.error = ("incorrect student id or password")
                     password.error = ("incorrect student id or password")
+                    var progressBar = findViewById<ProgressBar>(R.id.progressBar)
+                    progressBar.visibility = View.INVISIBLE
+                    var verify = findViewById<Button>(R.id.verify)
+                    verify.text = "VERIFY"
                 }
             }
 
@@ -72,52 +80,32 @@ class LoginScreen : AppCompatActivity() {
         })
     }
 
-    private fun verifyDriverInfo(parentActivity: AppCompatActivity,
-                                  loading: ProgressBar, verify: Button) : Thread {
-        return Thread {
-
-            var studentId = findViewById<EditText>(R.id.student_id)
-            var password = findViewById<EditText>(R.id.password)
-
-            val url = URL("http://10.0.2.2:5000/getDriverInformation?studentId="
-                    + studentId.text + "&password=" + password.text)
-
-            with(url.openConnection() as HttpURLConnection) {
-                requestMethod = "GET"  // optional default is GET
-
-                println("\nSent 'GET' request to URL : $url; Response Code : $responseCode")
-
-                val stringBuilder = StringBuilder()
-
-                inputStream.bufferedReader().use {
-                    it.lines().forEach { line ->
-                        stringBuilder.append(line)
-                    }
-                }
-
-                try {
-                    var response = JSONObject(stringBuilder.toString())
-                    var driverInformation = response.get("DriverInformation")
-                    if (driverInformation.javaClass.kotlin.qualifiedName != null) {
-                        var intent = Intent(parentActivity, MainDisplay::class.java)
-                        intent.putExtra("DriverInformation", driverInformation.toString())
-                        parentActivity.startActivity(intent)
-                    } else {
-                        runOnUiThread {
-                            kotlin.run {
-                                studentId.error = "Invalid Student Id or password"
-                                password.error = "Invalid Student Id or password"
-                                loading.visibility = View.INVISIBLE
-                                verify.isEnabled = true
-                                verify.text = "Verify"
-                            }
-                        }
-                    }
-
-                } catch (e: Exception) {
-
+    private fun getDriverInformation() {
+        var studentId = findViewById<EditText>(R.id.student_id)
+        var password = findViewById<EditText>(R.id.password)
+        val credential = Credential(studentId.text.toString(), password.text.toString())
+        val call = Client().getAPI().getDriverInformation(credential)
+        call!!.enqueue(object : Callback<Driver?> {
+            override fun onResponse(call: Call<Driver?>, response: Response<Driver?>) {
+                val driverInformation = response.body()?.driverInformation
+                if (driverInformation?.student_id != null) {
+                    var intent = Intent(this@LoginScreen, DriverDisplay::class.java)
+                    this@LoginScreen.startActivity(intent)
+                } else {
+                    studentId.error = ("incorrect student id or password")
+                    password.error = ("incorrect student id or password")
+                    var progressBar = findViewById<ProgressBar>(R.id.progressBar)
+                    progressBar.visibility = View.INVISIBLE
+                    var verify = findViewById<Button>(R.id.verify)
+                    verify.text = "VERIFY"
                 }
             }
-        }
+
+            override fun onFailure(call: Call<Driver?>?, t: Throwable) {
+                // setting text to our text view when
+                // we get error response from API.
+                Toast.makeText(applicationContext, t.message, Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 }
